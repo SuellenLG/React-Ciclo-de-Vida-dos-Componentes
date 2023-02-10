38af313fed10c0dd93791fe437ad0eabec1be3ea
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-02-09 23:36:17 -0300

    Observable project

---
+++ src/dados/categorias.js
@@ -0,0 +1,16 @@
+export default class Categorias{
+    constructor(){
+        this.categorias = [];
+        this._inscritos = [];
+    }
+    inscrever(func){
+        this.inscrever.push(func);
+    }
+
+    notificar(){
+        this._inscritos.forEach(func => func(this.categorias));
+    }
+    adicionarCategoria(novaCategoria){
+        this.categorias.push(novaCategoria);
+    }
+}
